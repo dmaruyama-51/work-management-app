0715d05d4bb94bae62e7a3d325fd6d863d2aa4e7
dmaruyama-51 <0501awasuram@gmail.com>
2022-03-05 15:37:55 +0900

    add UniqueConstraint date_unique_by_user

--- time_management_app/models.py
+++ time_management_app/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class TimeManagement(models.Model):
-    date = models.DateField('日付', unique=True)
+    date = models.DateField('日付')
     start_time = models.TimeField('始業時刻', null=True, blank=True)
     end_time = models.TimeField('就業時刻', null=True, blank=True)
     rest_time = models.FloatField('休憩時間', null=True, blank=True)
@@ -15,4 +15,12 @@ class TimeManagement(models.Model):
     updated_at = models.DateTimeField('更新日', auto_now_add=True)
 
     def __str__(self):
-        return str(self.date)
+        return str(self.created_by) + str(self.date)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['date', 'created_by'],
+                name = 'date_unique_by_user'
+            )
+        ]
