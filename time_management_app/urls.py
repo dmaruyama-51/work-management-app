378fc04c852190ef274c74af1f637c16fe30e139
dmaruyama-51 <0501awasuram@gmail.com>
2022-03-10 23:15:55 +0900

    add visualization

--- time_management_app/urls.py
+++ time_management_app/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path 
-from time_management_app import views 
+from django.urls import path
+from time_management_app import views
 
-app_name= 'time_management'
+app_name = "time_management"
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='index'),
-    path('new/', views.New.as_view(), name='new'),
-    path('detail/<int:id>/', views.Detail.as_view(), name='detail'),
-    path('detail/<int:id>/edit/', views.Edit.as_view() ,name='edit'),
-    path('detail/<int:pk>/delete/', views.Delete.as_view() ,name='delete'),
-    path('download/<str:month>', views.file_download, name='download'),
+    path("", views.Home.as_view(), name="index"),
+    path("new/", views.New.as_view(), name="new"),
+    path("detail/<int:id>/", views.Detail.as_view(), name="detail"),
+    path("detail/<int:id>/edit/", views.Edit.as_view(), name="edit"),
+    path("detail/<int:pk>/delete/", views.Delete.as_view(), name="delete"),
+    path("download/<str:month>", views.file_download, name="download"),
+    path("visualize/", views.Visualize.as_view(), name="visualize"),
 ]
